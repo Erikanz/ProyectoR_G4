9cecae875ef518e270b98037a4ffc271530f904f
Gaby <gabyvinueza18@gmail.com>
2023-08-04 23:34:51 -0500

    tarea especifica 1

--- modulo_R_grupo4.R
+++ modulo_R_grupo4.R
@@ -11,10 +11,15 @@ lapply(paquetes, library, character.only=TRUE)
 
 balance_2014_df<-read.xlsx("Data/balances_2014.xlsx")%>% view("balance_2014")
 
+str(balance_2014_df)
 
+#Tareas especificas
 
+empresas_df1<-balance_2014_df %>% transmute(Empresas= nombre_cia, Status= situacion, Tipo_de_empresa=tipo,
+                    País= pais, Provincia=provincia, Canton=canton, Ciudad= ciudad, 
+                    Actividad_economica= ciiu4_nivel1, Subactividad=ciiu4_nivel6) %>% view("empresas_df1")
 
-
+str(empresas_df1)
 
 
 
